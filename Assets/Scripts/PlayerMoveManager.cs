365e0da4371fc9784dd62d0b47e8f44e27ab183d
yokotate <yokosekazuya@yahoo.co.jp>
2021-06-25 18:12:16 +0900

    ジャンプ

--- Assets/Scripts/PlayerMoveManager.cs
+++ Assets/Scripts/PlayerMoveManager.cs
@@ -5,30 +5,63 @@ using UnityEngine;
 public class PlayerMoveManager : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float jumpPower = 10.0f;
     private Rigidbody2D rb;
     private Animator anim;
+    
+    private bool isJumpReady = false;
+    private float offsetY;
+    private float radius;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         anim = this.gameObject.GetComponent<Animator>();
+
+        offsetY = this.gameObject.GetComponent<CircleCollider2D>().offset.y;
+        radius = this.gameObject.GetComponent<CircleCollider2D>().radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        var horizontal = Input.GetAxis("Horizontal");
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
         if(horizontal==0){
             anim.SetBool("isRun", false);
-        }if(horizontal > 0){
-            this.transform.localScale = new Vector2(1,1);
-            anim.SetBool("isRun", true);
-        }if(horizontal < 0){
-            this.transform.localScale = new Vector2(-1,1);
+        }else{
+            this.transform.localScale = new Vector2(horizontal,1);
             anim.SetBool("isRun", true);
         }
+
+        if(Input.GetKeyDown("space")){
+            IsJumpReady();
+            if(isJumpReady){
+                isJumpReady = false;
+                Jump();
+            }
+        }
         
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    void IsJumpReady(){
+        var originPos = transform.position;
+        originPos = new Vector2(originPos.x - 0.5f, originPos.y - 1.01f);
+        RaycastHit2D hit = Physics2D.Raycast(originPos, transform.right, 1f);
+        if(hit.collider != null){
+            if(hit.collider.gameObject.CompareTag("Ground")){
+                isJumpReady = true;
+            }else{
+                isJumpReady = false;
+            }
+        }else{
+            isJumpReady = false;
+        }
+
+    }
+
+    void Jump(){
+        rb.AddForce(transform.up * jumpPower);
     }
 }
