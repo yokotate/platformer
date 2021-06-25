e848d1fd3a9d00fdb9fbb9b2cb9ab91ea6430440
yokotate <yokosekazuya@yahoo.co.jp>
2021-06-25 14:59:37 +0900

    アニメーションに関する追加

--- Assets/Scripts/PlayerMoveManager.cs
+++ Assets/Scripts/PlayerMoveManager.cs
@@ -6,17 +6,29 @@ public class PlayerMoveManager : MonoBehaviour
 {
     public float speed = 5.0f;
     private Rigidbody2D rb;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         rb = this.gameObject.GetComponent<Rigidbody2D>();
+        anim = this.gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var moveSpeed = Input.GetAxis("Horizontal") * speed;
+        var horizontal = Input.GetAxis("Horizontal");
 
-        rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        if(horizontal==0){
+            anim.SetBool("isRun", false);
+        }if(horizontal > 0){
+            this.transform.localScale = new Vector2(1,1);
+            anim.SetBool("isRun", true);
+        }if(horizontal < 0){
+            this.transform.localScale = new Vector2(-1,1);
+            anim.SetBool("isRun", true);
+        }
+        
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
     }
 }
